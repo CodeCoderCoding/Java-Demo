e63d32023c0f6589c9007bfa7aa639784cf2b516
codecodercoding <kneil5778@gmail.com>
2021-09-25 23:07:41 +0100

    添加ThreadLocal

---
+++ src/EConcurrency/Secure/ThreadLocal/ThreadLocalBasic.java
@@ -0,0 +1,23 @@
+package EConcurrency.Secure.ThreadLocal;
+
+/**
+ * ThreadLocal基本使用
+ */
+public class ThreadLocalBasic {
+	static ThreadLocal<Integer> local = new ThreadLocal<>();
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread child = new Thread() {
+			@Override
+			public void run() {
+				System.out.println("child thread initial: " + local.get());
+				local.set(200);
+				System.out.println("child thread final: " + local.get());
+			}
+		};
+		local.set(100);
+		child.start();
+		child.join();
+		System.out.println("main thread final: " + local.get());
+	}
+}
---
+++ src/EConcurrency/Secure/ThreadLocal/ThreadLocalDateFormat.java
@@ -0,0 +1,24 @@
+package EConcurrency.Secure.ThreadLocal;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ThreadLocalDateFormat {
+	static ThreadLocal<DateFormat> sdf = new ThreadLocal<DateFormat>() {
+
+		@Override
+		protected DateFormat initialValue() {
+			return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		}
+	};
+
+	public static String date2String(Date date) {
+		return sdf.get().format(date);
+	}
+
+	public static Date string2Date(String str) throws ParseException {
+		return sdf.get().parse(str);
+	}
+}
