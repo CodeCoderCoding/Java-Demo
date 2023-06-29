4219619c1e48a4afb0bf816df02f7fb2b4816912
CodeCoderCoding <kneil5778@gmail.com>
2023-06-29 08:36:07 +0800

    update CollectorsDemo.java

--- src/FDynamic/Stream/CollectorsDemo.java
+++ src/FDynamic/Stream/CollectorsDemo.java
@@ -16,6 +16,7 @@ public class CollectorsDemo {
                 new Person("bbb", 19, new BigDecimal(124))
         );
         groupBy(personList);
+        partionBy(personList);
     }
 
     /**
@@ -25,6 +26,16 @@ public class CollectorsDemo {
     public static void groupBy(List<Person> personList){
         Map<String, List<Person>> map=personList.stream()
                 .collect(Collectors.groupingBy(Person::getName));
+        System.out.println(map);
+    }
+
+    /**
+     * 根据人员的name是否是aaa来进行区分
+     * @param personList
+     */
+    public static void partionBy(List<Person> personList){
+        Map<Boolean, List<Person>> map=personList.stream()
+                .collect(Collectors.partitioningBy(person -> "aaa".equals(person.getName())));
         System.out.println(map);
     }
 
